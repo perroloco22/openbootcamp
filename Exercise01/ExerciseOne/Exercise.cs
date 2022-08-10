140edbca4271a7232f3156aa48c9f534093dfcff
Matias Emanuel <matu.93tkd@gmail.com>
2022-08-09 21:39:34 -0300

    complete exercise sesion 2

--- Exercise01/ExerciseOne/Exercise.cs
+++ Exercise01/ExerciseOne/Exercise.cs
@@ -19,7 +19,65 @@ namespace ExerciseOne
 
         public static void PrintHour()
         {
-            Console.WriteLine("This hour is: {0}",DateTime.Now.Hour.ToString());
+            Console.WriteLine("This hour is: {0}", DateTime.Now.Hour.ToString());
+        }
+
+
+        //Exercise session 2
+        public static void DataOfThePerson()
+        {
+            Console.WriteLine("Enter your Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your Lastname");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Enter your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("are you a programmer?");
+            string isProgrammer = Console.ReadLine();
+
+            Console.WriteLine($"Name: {name}\nLastname: {lastname}\nAge: {age}\nIs a programmer?: {isProgrammer}");
+
+        }
+
+        public static void DescriptionObjects()
+        {
+            int doors = 4;
+            int wheel = 4;
+            string mark = "Ford";
+            string validITV = "valid";
+            Console.WriteLine($"The car have {doors} doors, {wheel} wheels. The mark of the car is {mark}. The Itv is {validITV} ");
+
+            float weight = 60.2f;
+            float lenght = 2.4f; ;
+            string material = "metal";
+            string color = "red";
+
+            Console.WriteLine($"The features of the table:\n" +
+                $"weight: {weight}\n" +
+                $"lenght: {lenght}\n" +
+                $"material: {material}\n" +
+                $"color: {color}");
+
+        }
+
+        public static void CheckConditions(int number, char letter)
+        {
+            if (number >= 18)
+            {
+                Console.WriteLine("number valid");
+            }
+            if ('a' == letter)
+            {
+                Console.WriteLine("char valid");
+            }
+            if (number >= 18 && 'a' == letter)
+            {
+                Console.WriteLine("valid number and char");
+            }
+            if (number >= 18 || 'a' == letter)
+            {
+                Console.WriteLine("valid number or char");
+            }
         }
 
     }
